7fc71873fdcc9c329e8e9fad333f8f522cdc0e11
liangzhong.tan <liangzhong.tan@outlook.com>
2021-04-09 17:37:36 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,40 @@
 module.exports = {
   title: '以梅佐酒的个人空间',
   description: '修身 齐家 治国 平天下',
+  plugins: [
+    ['meting', {
+      //metingApi: "https://meting.sigure.xyz/api/music",
+      meting: {
+        // 网易
+        server: "netease",
+        // 读取歌单
+        type: "playlist",
+        mid: "6703421939",
+      },
+      // 不配置该项的话不会出现全局播放器
+      aplayer: {
+        // 吸底模式
+        fixed: true,
+        mini: true,
+        // 自动播放
+        autoplay: true,
+        // 歌曲栏折叠
+        listFolded: true,
+        // 颜色
+        theme: '#ff4d4f',
+        // 播放顺序为随机
+        order: 'random',
+        // 初始音量
+        volume: 0.2,
+        // 关闭歌词显示
+        lrcType: 0
+      },
+      mobile: {
+        // 手机端去掉cover图
+        cover: false,
+      }
+    }]
+  ],
   theme: 'reco',
   themeConfig: {
     type: 'blog',
